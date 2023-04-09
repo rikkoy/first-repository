5fce35da83e255bfd5a78177abecf08c38335474
rikkoy <rkkyaguchi@gmail.com>
2023-04-09 21:46:41 +0900

    webpack

--- udemy/typescript-lesson/webpack.prod.js
+++ udemy/typescript-lesson/webpack.prod.js
@@ -1,15 +1,16 @@
 const path = require('path');
+const { CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 console.log(__dirname);
 
 module.exports = {
+    mode: 'production',
     entry: './src/food-app/main.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/'
+        path: path.resolve(__dirname, 'dist')
     },
-    devtool: 'inline-source-map',
+    // devtool: 'inline-source-map',
     module: {
         rules: [{
             test: /\.ts$/,
@@ -19,5 +20,8 @@ module.exports = {
     },
     resolve: {
         extensions: ['.ts', '.js']
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 }
